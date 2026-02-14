066b5dee837a4042659fd9f18a333e5dcd7283ac
Yamada <your-email@example.com>
2026-02-14 09:14:56 +0000

    SEO: Add redirects for 404 pages + manifest.json

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,9 @@ const nextConfig = {
       { source: '/$', destination: '/', permanent: true },
       { source: '/blog/image-resize-sns-perfect-size', destination: '/blog', permanent: true },
       { source: '/blog/kakutei-shinkoku-receipt-pdf-2025', destination: '/blog', permanent: true },
+      { source: '/about/business', destination: '/about', permanent: true },
+      { source: '/convert/tsubo-converter', destination: '/convert', permanent: true },
+      { source: '/blog/pdf----149867', destination: '/blog', permanent: true },
     ]
   }
 }
